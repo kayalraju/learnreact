45ee3a2c32dd679a596291b000258b1f7db1eca0
kayalraju <87748199+kayalraju@users.noreply.github.com>
2023-11-06 10:28:10 +0530

    dd

--- src/formhandle/UserForm.jsx
+++ src/formhandle/UserForm.jsx
@@ -1,70 +1,94 @@
-import React, { useState } from 'react'
+import React,{useState,useEffect} from 'react'
 
-const UserForm = () => {
-    const initialstate={
-        name:"",
-        city:""
-    }
-    const[formvalue,setFormValue]=useState(initialstate)
+
+export default function UserForm() {
+    const initialValue={name: "",email: "",password: ""}
+    //get value from initialvalue 
+    const[formValues,setFormValues]=useState(initialValue)
+    //if no data found then call this state
     const[formError,setFormError]=useState({})
 
-    //form validate
-    const validate=(values)=>{
-        const error={};
+       
+    const handleChange=(e)=>{
+        //console.log(e.target);
+        const{name,value}=e.target
+        setFormValues({...formValues,[name]:value})
+        console.log(formValues);
+
+    }
+    const handleSubmit=(e)=>{
+        e.preventDefault()
+        setFormError(valiDate(formValues))
+       // setIsSubmit(true)
+    }
+
+    const valiDate=(values)=>{
+        const error={}
+        // for email validate check
+        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+        //for name validation condition
         if(!values.name){
             error.name="user name is required"
         }
-        if(!values.city){
-            error.city=" city name is required"
-        }
-
-        return error
-
+        //set email validation condition
+        if(!values.email){
+            error.email="user name is required"
+        }else if (!regex.test(values.email)) {
+            error.email = "This is not a valid email format!";
+          }
+          //set password validation condition
+        if(!values.password){
+            error.password="user name is required"
+        } else if (values.password.length < 4) {
+            error.password = "Password must be more than 4 characters";
+          } else if (values.password.length > 10) {
+            error.password = "Password cannot exceed more than 10 characters";
+          }
+        return error;
     }
+    return (
+        <>
+            <div className='container'>
+            
+                <h1>React from Validation</h1>
+                <form onSubmit={handleSubmit}>
+                    <div className="form-group">
+                        <label for="exampleInputEmail1">User Name</label>
+                        <input type="text"
+                         name='name'
+                          className="form-control"
+                           value={formValues.name} 
+                           onChange={handleChange}
+                           placeholder="Enter user name" />
+                           <p>{formError.name}</p>
+                    </div>
+                    <div className="form-group">
+                        <label for="exampleInputPassword1">Email</label>
+                        <input type="email"
+                         name='email'
+                          className="form-control"
+                           value={formValues.email}
+                           onChange={handleChange} 
+                           placeholder="Enter user email" />
+                           <p>{formError.email}</p>
+                    </div>
+                    <div className="form-group">
+                        <label for="exampleInputPassword1">Password</label>
+                        <input 
+                        type="password"
+                         name='password' 
+                         className="form-control" 
+                         value={formValues.password}
+                         onChange={handleChange} 
+                         placeholder="Enter user password" />
+                         <p>{formError.password}</p>
+                    </div>
 
-    const handleChange=(e)=>{
-        const {name,value}=e.target
-        setFormValue({...formvalue,[name]:value})
-        console.log(formvalue);
-    }
+                    <button type="submit" className="btn btn-success">Submit</button>
+                </form>
+            </div>
 
-    const handlesubmit=(e)=>{
-        e.preventDefault()
-        setFormError(validate(formvalue))
-        //console.log(formvalue);
-        
 
-    }
-  return (
-    <>
-    <div className="container">
-        <h1>User Form</h1>
-    <form onSubmit={handlesubmit}>
-  <div class="form-group">
-    <label for="exampleInputEmail1">Name</label>
-    <input type="text" class="form-control"
-    name='name'
-    value={formvalue.name}
-    onChange={handleChange}
-    />
-<b style={{color:"red"}}>{formError.name}</b>
-  </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">city</label>
-    <input type="text" class="form-control" 
-    name='city'
-    value={formvalue.city}
-    onChange={handleChange}
-    />
-    <b style={{color:"red"}}>{formError.city}</b>
-  </div>
-  
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
-    </div>
-      
-    </>
-  )
+        </>
+    )
 }
-
-export default UserForm
